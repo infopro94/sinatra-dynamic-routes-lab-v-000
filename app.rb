88b3fc7d4d55cc5827b4d82aa9ba20fe820c4e03
Susan Fowler <infopro94@gmail.com>
2017-12-21 03:19:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,18 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-  
+    operation =  params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+      if operation == "add"
+        answer = number1 + number2
+      elsif operation == "subtract"
+        answer = number1 - number2
+      elsif operation == "multiply"
+        answer = number1 * number2
+      elsif operation == "divide"
+        answer = number1/number2
+      end
   end
 
 
