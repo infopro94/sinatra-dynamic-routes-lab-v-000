df444bfdd20dfe4b672f592d4d8d80e9be947016
Susan Fowler <infopro94@gmail.com>
2017-12-21 03:16:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,12 +20,12 @@ class App < Sinatra::Base
   end
 
   get 'say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
-  # get "/hello/:name" do
-  #   @user_name = params[:name]
-  #   "Hello #{@user_name}!"
-  # end
+
+  get '/:operation/:number1/:number2' do
+  
+  end
 
 
 end
